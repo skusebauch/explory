48fdc2bcb91e69fe439b572ec9e7a7c29d2676ff
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-03 18:19:57 +0200

    basics of scroll animation

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+let controller;
+let slideScene;
+
+function animateSlides() {
+  // init controller
+  controller = new ScrollMagic.Controller();
+  // select some things
+  const sliders = document.querySelectorAll(".slide");
+  sliders.forEach((slide, index, slides) => {
+    const revealImg = slide.querySelector(".reveal-img");
+    //gsap - slideTl
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    //slideTl.fromTo(mainHeader, { y: "-100%" }, { y: "0%" }, "-=0.5");
+    // Create Scene Scroll Animation
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.8,
+    })
+      //.addIndicators({
+      //  colorStart: "black",
+      //  colorTrigger: "black",
+      //  name: "slide",
+      //})
+      .setTween(slideTl)
+      .addTo(controller);
+  });
+}
+animateSlides();
+
+//get rid of anchor at url
 $(document).ready(function () {
   const menuBtn = $("a");
 
